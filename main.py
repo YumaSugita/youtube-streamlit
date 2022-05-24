abc34cc3893ca8d73b6b42636d8d2b202abf5b0f
YumaSugita <61612887+Nakushio@users.noreply.github.com>
2022-05-24 16:02:35 +0900

    5th commit

--- main.py
+++ main.py
@@ -12,20 +12,35 @@ bar = st.progress(0)
 for i in range(100):
 	latest_iteration.text(f'Iteration {i+1}')
 	bar.progress(i + 1)
-	time.sleep(0.1)
+	time.sleep(0.01)
 
 'Done!!!!!!'
 
 left_column, right_column = st.columns(2)
-button = left_column.button('右カラムに文字を表示')
+button = left_column.button('発射！')
 if button:
-	right_column.write('ここは右カラム')
+	st.balloons()
+	
 
 text = st.text_input('あなたの趣味を教えてください。')
-condition = st.slider('あなたの調子は？', 0, 100, 50)
+condition = st.slider('あなたの調子は？', 1, 5, 3)
 
 'あなたの趣味：', text
 'コンディション：', condition
+
+if condition == 5:
+	food = st.radio(
+		"いま食べたいのは？",
+		('肉','魚','ラーメン')
+	)
+
+	if food == '肉':
+		'今日は焼肉！'
+	elif food == '魚':
+		'寿司行こう'
+	elif food == 'ラーメン':
+		'もちろんとんこつだよね？'
+	
 
 expander1 = st.expander('問い合わせ1')
 expander1.write('問い合わせ1の回答')
